a23c3e7bd773b5214edf612f4101c8adb97a1727
djstalca <120321133+djstalca@users.noreply.github.com>
2026-09-07 11:36:44 +0200

    feat: register saved places repository

--- app/src/main/java/si/lukabencina/kilometrina/KilometrinaApplication.kt
+++ app/src/main/java/si/lukabencina/kilometrina/KilometrinaApplication.kt
@@ -2,6 +2,7 @@ package si.lukabencina.kilometrina
 
 import android.app.Application
 import si.lukabencina.kilometrina.data.AppDatabase
+import si.lukabencina.kilometrina.data.SavedPlaceRepository
 import si.lukabencina.kilometrina.data.SettingsRepository
 import si.lukabencina.kilometrina.data.TripRepository
 
@@ -9,4 +10,5 @@ class KilometrinaApplication : Application() {
     val database by lazy { AppDatabase.create(this) }
     val tripRepository by lazy { TripRepository(this, database.tripDao()) }
     val settingsRepository by lazy { SettingsRepository(this) }
+    val savedPlaceRepository by lazy { SavedPlaceRepository(this) }
 }

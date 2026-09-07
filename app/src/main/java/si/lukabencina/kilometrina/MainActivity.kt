4c425e5548d969784b6bd5d868b03fb7136672a7
djstalca <120321133+djstalca@users.noreply.github.com>
2026-09-07 20:13:45 +0200

    feat: install splash screen

--- app/src/main/java/si/lukabencina/kilometrina/MainActivity.kt
+++ app/src/main/java/si/lukabencina/kilometrina/MainActivity.kt
@@ -3,12 +3,14 @@ package si.lukabencina.kilometrina
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import si.lukabencina.kilometrina.ui.KilometrinaApp
 import si.lukabencina.kilometrina.ui.theme.KilometrinaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {

2962b3c0d6d8be16182b12a50cd9cc66ff54b638
djstalca <120321133+djstalca@users.noreply.github.com>
2026-09-07 20:07:11 +0200

    feat: wire smart place repository into trips

--- app/src/main/java/si/lukabencina/kilometrina/KilometrinaApplication.kt
+++ app/src/main/java/si/lukabencina/kilometrina/KilometrinaApplication.kt
@@ -10,10 +10,16 @@ import si.lukabencina.kilometrina.data.VehicleRepository
 
 class KilometrinaApplication : Application() {
     val database by lazy { AppDatabase.create(this) }
-    val tripRepository by lazy { TripRepository(this, database.tripDao()) }
     val settingsRepository by lazy { SettingsRepository(this) }
     val savedPlaceRepository by lazy { SavedPlaceRepository(this) }
     val vehicleRepository by lazy { VehicleRepository(this) }
+    val tripRepository by lazy {
+        TripRepository(
+            context = this,
+            dao = database.tripDao(),
+            savedPlaceRepository = savedPlaceRepository,
+        )
+    }
     val backupRepository by lazy {
         BackupRepository(
             database = database,
